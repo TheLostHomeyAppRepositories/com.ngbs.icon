a640c8eed9503e80a592f65cb6d20c1f0f40904f
Gábor Molnár <894421+molnarg@users.noreply.github.com>
2024-01-03 11:27:38 +0100

    Small tweaks for pairing

--- drivers/icon/driver.ts
+++ drivers/icon/driver.ts
@@ -3,25 +3,22 @@ import { PairSession } from 'homey/lib/Driver';
 import { NgbsIconModbusTcpClient } from "ngbs-icon";
 
 class IconDriver extends Homey.Driver {
-
-  /**
-   * onInit is called when the driver is initialized.
-   */
   async onInit() {
-    this.log('MyDriver has been initialized');
+    this.log('Initialized');
   }
 
   async onPair(session: PairSession) {
     let address: string;
 
-    session.setHandler("setAddress", async msg => {
+    session.setHandler("set_address", async msg => {
+      this.log('Set address to ' + address);
       address = msg;
     });
 
     session.setHandler("list_devices", async () => {
-      const client = new NgbsIconModbusTcpClient(address);
       try {
-        this.log('Testing address ' + address);
+        this.log('Connecting to ' + address);
+        const client = new NgbsIconModbusTcpClient(address);
         const thermostats = await client.getThermostats();
         this.log('Successfully retrieved ' + thermostats.length + ' thermostats.');
         const devices = thermostats.map((thermostat, index) => ({
@@ -32,10 +29,11 @@ class IconDriver extends Homey.Driver {
           },
         }));
         this.log('Devices:', devices);
+        client.disconnect();
         return devices;
       } catch (e: any) {
-        const code: string = e.code || e.data.code;
-        const error = this.homey.__("pair.address.errors." + code) || e.message || e.data.message || JSON.stringify(e);
+        const code: string = e?.code || e?.data?.code || 'other';
+        const error = this.homey.__("pair.address.errors." + code) || e?.message || e?.data?.message || JSON.stringify(e);
         this.log('NGBS client error', code, error, e);
         throw new Error(error);
       }
